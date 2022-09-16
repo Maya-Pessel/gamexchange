a6ed84fdacc51eebf11268c1bd7051456d208d0d
maya.pessel <maya.pessel@gmail.com>
2022-09-16 09:58:17 +0200

    Create product entity and add products to the home page

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Entity\Product;
+namespace App\Controller;
+
+use App\Repository\ProductRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_home", methods="GET")
+     */
+    public function index(ProductRepository $productRepository): Response
+    {
+        $products = $productRepository->findAll();
+
+        return $this->render('product/index.html.twig', [
+            'products' => $products
+        ]);
+    }
+
+}
