6b9bee8a755adf341c30c610c5247f180a9ab13d
maya.pessel <maya.pessel@gmail.com>
2022-09-22 16:04:07 +0200

    add create product feature and styling /create page

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,8 +3,12 @@
 use App\Entity\Product;
 namespace App\Controller;
 
+use App\Entity\Product;
+use App\Form\ProductType;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,6 +23,32 @@ class ProductController extends AbstractController
 
         return $this->render('/index.html.twig', [
             'products' => $products
+        ]);
+    }
+        /**
+     * @Route("/create", name="app_create_product", methods="GET|POST")
+     */
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        $product = new Product;
+
+        $form = $this -> createForm(ProductType::class, $product);
+
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()){
+            // $product -> setUser($this->security->getUser());
+            $em->persist($product);
+            $em->flush();
+
+            $this->addFlash('success', 'Product successfully created');
+
+            return $this->redirectToRoute("app_home");
+        }
+
+        return $this -> render('product/create.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 
