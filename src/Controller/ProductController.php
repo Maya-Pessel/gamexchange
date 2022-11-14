1ac0cfe493a5d03a700587b6f895ba2afb25fb37
maya.pessel <maya.pessel@gmail.com>
2022-11-14 18:08:14 +0100

    modification of css and twig

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -18,7 +18,7 @@ class ProductController extends AbstractController
      */
     public function index(ProductRepository $productRepository): Response
     {
-        $products = $productRepository->findAll();
+        $products = $productRepository->findBy([], ['id' => 'DESC']);
 
         return $this->render('/index.html.twig', [
             'products' => $products
