5b35bec052db31dd7d237b40e90084a1c04b3fce
maya.pessel <maya.pessel@gmail.com>
2022-11-16 18:08:46 +0100

    change nav if user isnt logged

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -33,8 +33,10 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
+            // send email when user is created
+            $this->addFlash('success', 'User successfully created');
 
-            return $this->redirectToRoute('app_home');
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('registration/register.html.twig', [
